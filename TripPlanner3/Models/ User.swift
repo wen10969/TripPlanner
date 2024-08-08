968872879d10cd43aa343cd75bfe7981e020a0d0
hinalp02 <hinalp2002@gmail.com>
2024-08-07 19:42:46 -0700

    Initial commit to hinal branch

---
+++ TripPlanner3/Models/ User.swift
@@ -0,0 +1,14 @@
+//
+//   User.swift
+//  TripPlanner3
+//
+//  Created by stlp on 8/7/24.
+//
+
+import Foundation
+
+struct User {
+    let username: String
+    let email: String
+    let userUID: String
+}
---
+++ TripPlanner3/Models/RegiserUserRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RegiserUserRequest.swift
+//  TripPlanner3
+//
+//  Created by stlp on 8/7/24.
+//
+
+import Foundation
+
+struct RegiserUserRequest {
+    let username: String
+    let email: String
+    let password: String
+}
